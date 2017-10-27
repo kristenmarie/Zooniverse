31c15b23d70ae9eafb92b076e41dd0dc7d9ec0d8
Kristen Kulha <kristen.m.kulha@gmail.com>
2017-10-27 13:08:24 -0700

    Add styling.

--- app/animal-list.component.ts
+++ app/animal-list.component.ts
@@ -9,34 +9,36 @@ import { Animal } from './animal.model';
     <option value="young">Young Animals (< 2 Years of Age)</option>
     <option value="mature">Mature Animals (>= 2 years of age)</option>
   </select>
-  <table class="striped centered responsive-table">
-    <thead>
-      <th class="species">Species</th>
-      <th class="name">Name</th>
-      <th class="age">Age</th>
-      <th class="diet">Diet</th>
-      <th class="location">Location</th>
-      <th class="caretakers">Caretakers Needed</th>
-      <th class="sex">Sex</th>
-      <th class="likes">Likes</th>
-      <th class="dislikes">Dislikes</th>
-      <th>Edit</th>
-    </thead>
-    <tbody>
-      <tr *ngFor="let currentAnimal of childAnimalList | age:filterByAge">
-        <td class="species">{{currentAnimal.species}}</td>
-        <td class="name">{{currentAnimal.name}}</td>
-        <td class="age">{{currentAnimal.age}}</td>
-        <td class="diet">{{currentAnimal.diet}}</td>
-        <td class="location">{{currentAnimal.zooLocation}}</td>
-        <td class="caretakers">{{currentAnimal.caretakersNeeded}}</td>
-        <td class="sex">{{currentAnimal.sex}}</td>
-        <td class="likes">{{currentAnimal.like}}</td>
-        <td class="dislikes">{{currentAnimal.dislike}}</td>
-        <td><button class="btn" (click)="editButtonHasBeenClicked(currentAnimal)">Edit</button></td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="card-panel">
+    <table class="striped centered responsive-table">
+      <thead>
+        <th class="species">Species</th>
+        <th class="name">Name</th>
+        <th class="age">Age</th>
+        <th class="diet">Diet</th>
+        <th class="location">Location</th>
+        <th class="caretakers">Caretakers Needed</th>
+        <th class="sex">Sex</th>
+        <th class="likes">Likes</th>
+        <th class="dislikes">Dislikes</th>
+        <th>Edit</th>
+      </thead>
+      <tbody>
+        <tr *ngFor="let currentAnimal of childAnimalList | age:filterByAge">
+          <td class="species">{{currentAnimal.species}}</td>
+          <td class="name">{{currentAnimal.name}}</td>
+          <td class="age">{{currentAnimal.age}}</td>
+          <td class="diet">{{currentAnimal.diet}}</td>
+          <td class="location">{{currentAnimal.zooLocation}}</td>
+          <td class="caretakers">{{currentAnimal.caretakersNeeded}}</td>
+          <td class="sex">{{currentAnimal.sex}}</td>
+          <td class="likes">{{currentAnimal.like}}</td>
+          <td class="dislikes">{{currentAnimal.dislike}}</td>
+          <td><button class="btn" (click)="editButtonHasBeenClicked(currentAnimal)">Edit</button></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
   `
 })
 
