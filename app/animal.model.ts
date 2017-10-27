3e644a51c6319b331ed2f8aec519fc0edbee0d81
Kristen Kulha <kristen.m.kulha@gmail.com>
2017-10-27 09:02:16 -0700

    Add animal model.

---
+++ app/animal.model.ts
@@ -0,0 +1,3 @@
+export class Animal {
+  constructor(public species: string, public name: string, public age: number, public diet: string, public zooLocation: string, public caretakersNeeded: number, public sex: string, public like: string, public dislike: string) { }
+}
